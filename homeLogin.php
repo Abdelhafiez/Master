cfc2a0e3c1eb53ee46c2ad7c9a13fe09cd3e4a66
MostafaAbass <engmostafa92@gmail.com>
2015-01-03 19:35:15 +0200

    FormLogin

---
+++ homeLogin.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>
+		Login 
+	</title>
+</head>
+<body>
+	<form method="post" action = "login.php">
+		Name: <input type = "text" name = "UserName" ><br / >
+		Password: <input type = "password" name = "Password"> <br / >
+		Remeber me ? :<input type = "checkbox" name = "remember"><br / >
+		<input type = "submit" name = "submit" value = "Login">
+	</form>
+
+</body>
+</html>
