ceba864e1f4ea305574248ac443673e28e3d897b
Abdelhafiez <abdelhafiez8080@gmail.com>
2015-01-03 17:39:35 +0200

    add Question

---
+++ addQ.php
@@ -0,0 +1,16 @@
+<?php
+	include("myphp.php");
+?>
+<html>
+	<head>
+		<title>Add Question</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="viewprofile.php">View Profile</a></li>
+			<li><a href="logout.php">LogOut</a></li>
+			
+		</ul>
+	</body>
+</html>
