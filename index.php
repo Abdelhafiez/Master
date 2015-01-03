60014568fe2b9e5e3e1e47cc21dd20b37bca4440
Abdelhafiez <abdelhafiez8080@gmail.com>
2015-01-03 17:27:10 +0200

    home

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+Include("myphp.php");
+?>
+<html>
+	<head>
+		<title>Math Test</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href = "login.php">Login</a></li>
+			<li><a href = "register.php">Register</li>
+			<li><a href = "viewprofile.php">View Profile</li>
+			<li><a href = "logout.php">Logout</li>
+		</ul>
+	</body>
+</html>
