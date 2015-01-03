5a406329b1359b852fc70023c3b23029c19cdd7f
Abdelhafiez <abdelhafiez8080@gmail.com>
2015-01-03 17:33:36 +0200

    register

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php
+Include("myphp.php");
+?>
+<html>
+	<head>
+		<title>Register</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href = "index.php">Home</a></li>
+			<li><a href = "login.php">Login</a></li>
+			<li><a href = "viewprofile.php">View Profile</li>
+			<li><a href = "logout.php">Logout</li>
+		</ul>
+	</body>
+</html>
