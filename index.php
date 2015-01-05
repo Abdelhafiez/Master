60955a8eaed782c6515cf8351dab82ea67ad954e
Abdelhafiez <abdelhafiez8080@gmail.com>
2015-01-05 09:43:43 +0200

    commit

--- index.php
+++ index.php
@@ -1,26 +1,46 @@
+<?php
+
+include_once('validation.php');
+
+session_start();
+
+?>
 
 <html>
 	<head>
 		<title>Math Test</title>
+		<style>
+			ul {
+				list-style-type: none;
+				margin: 0;
+				padding: 0;
+			}
+			li {
+				display: inline;
+			}
+		</style>
 	</head>
 	<body>
 		<?php
-			session_start();
 			if(isset($_SESSION['Username'])){ 
 				if ($_SESSION['Type'] == "Student"){ // student
-					echo "&nbsp <a href = 'index.php'>Home</a><br/><br/>\n";
-					echo "&nbsp <a href = 'viewprofile.php'>View Profile</a><br/><br/>\n";
-					echo "&nbsp <a href = 'choosetest.php'>Choose Test</a><br/><br/>\n";
-					echo "&nbsp <a href = 'logout.php'>LogOut</a><br/><br/>\n";
+					echo "<ul>";
+					echo "<li><a href = 'index.php'>Home</a></li>";
+					echo "&nbsp&nbsp<li><a href = 'viewprofile.php'>View Profile</a></li>";
+					echo "&nbsp&nbsp<li><a href = 'choosetest.php'>Choose Test</a></li>";
+					echo "&nbsp&nbsp<li><a href = 'logout.php'>LogOut</a><br/><br/></li>";
+					echo "</ul>";
 				}
-				else { // Educator
-					echo "&nbsp <a href = 'index.php'>Home</a><br/><br/>\n";
-					echo "&nbsp <a href = 'viewprofile.php'>View Profile</a><br/><br/>\n";
-					echo "&nbsp <a href = 'viewQuizzes.php'>My Quizzes</a>\n";
-					echo "&nbsp <a href = 'logout.php'>LogOut</a><br/><br/>\n";
+				else { // Educatorz
+					echo "<ul>";
+					echo "<li><a href = 'index.php'>Home</a></li>";
+					echo "&nbsp&nbsp<li><a href = 'viewprofile.php'>View Profile</a></li>";
+					echo "&nbsp&nbsp<li><a href = 'viewQuizzes.php'>My Quizzes</a></li>";
+					echo "&nbsp&nbsp<li><a href = 'logout.php'>LogOut</a></li>";
+					echo "</ul>";
 				}
 			}else{ 
-				header("location:login.html");
+				header("location:loginForm.php");
 			}
 		?>
 	</body>
